d6fe02db8918e4976f031b8ffdadb6921520efd6
Noboru Ota <me@nobiot.com>
2020-04-16 18:01:04 +0200

    (feat) Add advice to extract links (roam and cite)
    TODO: Remove pandoc dependency.
    The implementation uses a varianble defined in pandoc-mode.
    This variable is just a regexp; copy and paste it with reference
    to the original. Of course, there are more than one  regrex what we need to support.

--- md-roam.el
+++ md-roam.el
@@ -45,22 +45,13 @@ It assumes:
  (3) The format is 'title: The Document Title Value'
  (4) The title value is escaped by the double quotations
 
-The extraction is done via regex expresion in defined in 'md-roam-title-regex.
+The extraction is done via regex expresion in the variable defined in
+'md-roam-title-regex.
 It expects one or more space after the 'title:' key before the title value.
 If the space is not there, the title extracted will be ':title value'.
 
 At the moment, for some weird reason, the regex leaves one whitespace in front
-of the title. 's-trim-left is used to remove it.
-
-
-TODO At the moment, an empty title is fine, but if there is no space or one
-     space, it returns ':' as the title; if more than one space, ''.
-TODO Bind the regexp search for the tile. At the moment, the title key: value
-     does not even have to be wihtin the YAML frontmatter, as long as there is
-     '---' at the top of the file!
-TODO Ideally, I do not want s-trim-left to be there, but I could not figure
-     out how.
-I will move these issues to GitHub/GitLab."
+of the title. 's-trim-left is used to remove it."
 
   (when
     (save-excursion
@@ -77,6 +68,80 @@ I will move these issues to GitHub/GitLab."
 
 (advice-add 'org-roam--extract-titles :filter-return #'md-roam--extract-titles)
 
+(defun md-roam--extract-wiki-links (file-path)
+  "Extract links in the form of [[link]].
+FILE-PATH is mandatory as org-roam--extract-links identifies it."
+  (let (md-links)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "\\[\\[\\([^]]+\\)\\]\\]" nil t)
+        (let* ((to-file (concat (match-string-no-properties 1) ".md"))
+               (end (match-end 1))
+               (begin-of-block)
+               (end-of-block)
+               (content)
+               (link-type "roam"))
+          (when (f-file-p to-file)
+            ;; get the text block = content around the link as context
+            (forward-sentence)
+            (setq end-of-block (point))
+            (backward-sentence)
+            (setq begin-of-block (point))
+            (setq content (buffer-substring-no-properties begin-of-block end-of-block))
+            (goto-char end) ; move back to the end of the regexp for the loop
+            (setq md-links
+                  (append md-links
+                          (list (vector file-path ; file-from
+                                        (file-truename (expand-file-name to-file (file-name-directory file-path))) ; file-to
+                                        link-type ;
+                                        (list :content content :point begin-of-block))))))))) ; properties
+      md-links))
+
+(defun md-roam--extract-cite-links (file-path)
+  "Extract cites defined by @bibkey.
+FILE-PATH is mandatory as org-roam--extract-links identifies it."
+  (let (md-cite-links)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward pandoc-regex-parenthetical-citation-single nil t)
+        (let* ((to-file (match-string-no-properties 3))
+               (end (match-end 1))
+               (begin-of-block)
+               (end-of-block)
+               (content)
+               (link-type "cite"))
+          (when to-file)
+          (forward-sentence)
+          (setq end-of-block (point))
+          (backward-sentence)
+          (setq begin-of-block (point))
+          (setq content (buffer-substring-no-properties begin-of-block end-of-block))
+          (goto-char end) ; move back to the end of the regexp for the loop
+          (setq md-cite-links
+                (append md-cite-links
+                        (list (vector file-path ; file-from
+                                      to-file
+                                      link-type
+                                      (list :content content :point begin-of-block))))))))  ; properties
+    md-cite-links))
+
+(defun md-roam--extract-links (orginal-extract-links &optional file-path)
+  "Add markdown links (wiki and cite) for FILE-PATH to org-roam the equivalent.
+ORGINAL-EXTRACT-LINKS is supplemented with md-roam functions.
+It should be used in 'advice-add'."
+  (let* ((file-path (or file-path
+                        (file-truename (buffer-file-name))))
+         (links (apply orginal-extract-links '(file-path)))
+         (md-links (md-roam--extract-wiki-links file-path))
+         (md-cite-links (md-roam--extract-cite-links file-path)))
+                                        ;TODO remove pandoc-mode dependency
+    (when md-links
+      (setq links (append md-links links)))
+    (when md-cite-links
+      (setq links (append md-cite-links)))
+    links))
+
+(advice-add 'org-roam--extract-links :around #'md-roam--extract-links)
 
 (provide 'md-roam)
 ;;; md-roam.el ends here
